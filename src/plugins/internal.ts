2a8331d6b7f4444ff57f540e89759e487c3356d7
Saahil <neon@saahild.com>
2024-10-27 17:46:15 -0400

    feat(tree-wide): init

---
+++ src/plugins/internal.ts
@@ -0,0 +1,15 @@
+/**
+ * All plugins exported here are internal plugins.
+ * When building on your own you can configure them here.
+ * For default build, these will all be FORCED turned on and cant be turned off.
+ */
+export default [{
+    name: "Owner Badges", 
+    description: "Adds a little cool badge to the owner of this plugin :P",
+    async execute() {
+        // what Actually code should look like
+        /**
+         * CreateAdditonalUserBadge(':crown:', 'Owner', 'This plugin is owned by the owner of this plugin :P', (e) => e.user.displayName ==  )
+             */
+    }
+}]
