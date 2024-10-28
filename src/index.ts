5a890ebdd59d13996fcf05a4d4b4bff7af74c093
Saahil <neon@saahild.com>
2024-10-27 21:51:56 -0400

    update: oneko works

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import internal from "./plugins/internal";
+import misc from "./plugins/misc";
 import * as utils from "./util";
 import JsCookie from "js-cookie";
 // bind console.log to
@@ -8,17 +9,22 @@ console.log = (...arguments) => {
   log0(`[SURAKKU/${utils.getTime()}]`, ...arguments);
 };
 console.log(`Surakku LOADED :P`);
-console.log(JsCookie.get());
+
+// remove CSP
+document.querySelector('meta[http-equiv]')?.remove()
+// console.log(JsCookie.get());
 //@ts-ignore
-console.log(
-  browser.cookies.get({
-    name: "d",
-    url: "https://app.slack.com",
-  }),
-);
-const plugins = [internal];
-for (const pluginCat of plugins) {
-  for (const plugin of pluginCat) {
-    plugin.execute();
-  }
-}
+// console.log(
+//   browser.cookies.get({
+//     name: "d",
+//     url: "https://app.slack.com",
+//   }),
+// );
+const plugins = [internal, misc];
+window.addEventListener('load', () => {
+  for (const pluginCat of plugins) {
+    for (const plugin of pluginCat) {
+      plugin.execute();
+    }
+  }  
+})
